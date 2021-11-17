a6d5efffadfc433ad89a03bf09cc21d26eccbad2
shubh996 <67875662+shubh996@users.noreply.github.com>
2021-11-17 22:07:53 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,33 +3,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Navigate_Whatsapp from './Navigate_Whatsapp';
 import * as serviceWorker from './serviceWorker';
 import Display_Product from './Display_Product';
 import queryString from 'query-string'
+import firebase from "firebase"
 
+const gotoproduct =() =>{
 
-
-
-const goto =() =>{
-
-  const query = queryString.parse(window.location.search)
+const query = queryString.parse(window.location.search)
 const uid = query.p_uid
 const p_uid = uid.split("/")
 const b_uid= uid.split("b_uid")
 
-return <Display_Product p_uid={p_uid[0]} b_uid={b_uid[1]} />
+
+
+return( <Display_Product p_uid={p_uid[0]} b_uid={b_uid[1]} />)
 
 }
+
+const gotohome =() =>{
+
+      const query = queryString.parse(window.location.search) 
+      var x = window.location.href.split(".")[0].split("//")[1] 
+
+      
+      
+      if(x == "whatsapp"){
+        return( <Navigate_Whatsapp  /> )
+      }
+      else{
+        return( <App    b_uid={x}  /> )
+      }
+  
+  
+  }
 
 
 
 ReactDOM.render(
-  
+
   <React.StrictMode>
     {
       window.location.search.includes("p_uid") 
-      ?goto()
-      :<App b_uid={window.location.pathname} />
+      ?gotoproduct()
+      :gotohome()
     }
     
   </React.StrictMode>,
